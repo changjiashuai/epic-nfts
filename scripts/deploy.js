39d298044915f64b19ecf4042b6cc2a0ab91d775
changjiashuai <changjiashuai@gmail.com>
2022-10-05 13:36:19 +0800

    deploy nft contract & minted nft #1, #2. https://testnets.opensea.io/0xC65Bb957AFddEf217bC2db4C3da87638FBAB175f

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,25 +7,24 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+    const nftContractFactory = await hre.ethers.getContractFactory("MyEpicNFT");
+    const nftContract = await nftContractFactory.deploy();
+    await nftContract.deployed();
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+    console.log("Contract deployed to:", nftContract.address);
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+    let txn = await nftContract.makeAnEpicNFT();
+    await txn.wait();
+    console.log("Minted NFT #1");
 
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+    txn = await nftContract.makeAnEpicNFT();
+    await txn.wait();
+    console.log("Minted NFT #2");
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+    console.error(error);
+    process.exitCode = 1;
 });
